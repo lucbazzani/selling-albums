55d2f8e3734a41d5fd25e2193a93d8cbaa1765f4
lucbazzani <lu.bazzani@hotmail.com>
2026-04-19 16:14:29 -0300

    Implement List Favourite albums on HomeViewModel

--- SellingAlbums/Controllers/HomeController.cs
+++ SellingAlbums/Controllers/HomeController.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SellingAlbums.Models;
+using SellingAlbums.Repositories.Interfaces;
+using SellingAlbums.ViewModels;
 using System.Diagnostics;
 
 namespace SellingAlbums.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IAlbumRepository _albumRepository;
+
+        public HomeController(IAlbumRepository albumRepository)
+        {
+            _albumRepository = albumRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var homeVM = new HomeViewModel
+            {
+                FavouriteAlbums = _albumRepository.FavouriteAlbums
+            };
+
+            return View(homeVM);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ SellingAlbums/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using SellingAlbums.Models;
+
+namespace SellingAlbums.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Album> FavouriteAlbums { get; set; }
+    }
+}
