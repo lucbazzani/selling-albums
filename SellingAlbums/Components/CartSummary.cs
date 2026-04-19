6eeca3b3c0cf4194b2ce9927c2d3d8b850f2d115
lucbazzani <lu.bazzani@hotmail.com>
2026-04-19 17:21:26 -0300

    Display Cart

---
+++ SellingAlbums/Components/CartSummary.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SellingAlbums.Models;
+using SellingAlbums.ViewModels;
+
+namespace SellingAlbums.Components
+{
+    public class CartSummary : ViewComponent
+    {
+        public readonly ShoppingCart _shoppingCart;
+
+        public CartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartVM = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                CartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartVM);
+        }
+    }
+}
